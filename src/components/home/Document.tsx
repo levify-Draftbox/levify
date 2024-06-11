0eb01e01acf27efd3f91e9f82ff14ef018afd2bf
p4rthk4 <hello@parthka.dev>
2024-06-11 18:47:37 +0530

    [add] docker-compose.yml

--- src/components/home/Document.tsx
+++ src/components/home/Document.tsx
@@ -2,7 +2,7 @@ const Document = () => {
   return (
     <div className='flex justify-center items-center flex-col p-2'>
         <h1>document</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, distinctio, minus iure quam aliquam vitae molestias dicta temporibus illum ut ratione nisi dolorem dignissimos dolores quidem alias similique eius placeat.</p>
+        <p>Loremq ipsum dolor, sit amet consectetur adipisicing elit. Ad, distinctio, minus iure quam aliquam vitae molestias dicta temporibus illum ut ratione nisi dolorem dignissimos dolores quidem alias similique eius placeat.</p>
     </div>
   )
 }
