ae4c37ac08928628e329a46699a7dbc8c772b497
ravi <italiyaravi704@gmail.com>
2024-06-11 17:14:08 +0530

    [add]-document

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import './app.css'
-import Home from './components/home/Home'
+import React from 'react';
+import './app.css';
+import Home from './components/home/Home';
+import { Routes, Route } from 'react-router-dom';
+import Page from './components/home/Page';
 
 function App() {
   return (
-    <>
-      <div>
-        <Home/>
-      </div>
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/page1" element={<Page />} />
+        <Route path="/page2" element={<Page />} />
+        <Route path="/page3" element={<Page />} />
+        <Route path="/page4" element={<Page />} />
+
+
+      </Routes>
+  );
 }
 
-export default App
+export default App;
