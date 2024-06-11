b980cd0a7224ae14768bb741f1f61c250fe375dd
ravi <italiyaravi704@gmail.com>
2024-06-11 11:28:13 +0530

    [add]-theam

--- src/components/home/SideBar.tsx
+++ src/components/home/SideBar.tsx
@@ -1,36 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import { GoHome } from "react-icons/go";
+import { IoHomeOutline } from "react-icons/io5";
 import { IoSettingsOutline } from "react-icons/io5";
-import { BiLogOut } from "react-icons/bi";
+import { IoLogOutOutline } from "react-icons/io5";
+import { IoSunnyOutline } from "react-icons/io5";
+import { IoMoonOutline } from "react-icons/io5";
 
 const SideBar = () => {
+  const [ToggleTheme, setToggleTheme] = useState(false);
   return (
-    <div className="flex flex-col justify-between h-full">
+    <div className="flex flex-col justify-between h-full dark:bg-black">
       <div className="w-full flex flex-col mt-5">
-        <div className="flex w-full px-3 py-4 h-7 rounded-md hover:bg-gray-200">
-          <div className="flex items-center justify-center gap-2">
-            <IoSearch fontSize={23} />
+        <div className="flex w-full px-3 py-4 h-10 rounded-md hover:bg-gray-200">
+          <div className="flex items-center justify-center gap-3">
+            <IoSearch fontSize={22} />
             <h1 className="text-lg">Search</h1>
           </div>
         </div>
         <div className="flex w-full px-3 py-4 h-10 rounded-md hover:bg-gray-200">
-          <div className="flex items-center justify-center gap-2">
-            <GoHome fontSize={25} />
+          <div className="flex items-center justify-center gap-3">
+            <IoHomeOutline fontSize={22} />
             <h1 className="text-lg">Home</h1>
           </div>
         </div>
       </div>
+
       <div className="w-full flex flex-col mb-2">
+        <div
+          onClick={() => setToggleTheme(!ToggleTheme)}
+          className="flex w-full px-3 py-4 h-10 rounded-md hover:bg-gray-200"
+        >
+          <div className="flex items-center justify-center gap-3">
+            {ToggleTheme ? (
+              <IoSunnyOutline fontSize={23} />
+            ) : (
+              <IoMoonOutline fontSize={23} />
+            )}
+            {ToggleTheme ? (
+              <h1 className="text-lg">Light</h1>
+            ) : (
+              <h1 className="text-lg">Dark</h1>
+            )}
+          </div>
+        </div>
         <div className="flex w-full px-3 py-4 h-10 rounded-md hover:bg-gray-200">
-          <div className="flex items-center justify-center gap-2">
-            <BiLogOut fontSize={25} />
+          <div className="flex items-center justify-center gap-3">
+            <IoLogOutOutline fontSize={22} />
             <h1 className="text-lg">Logout</h1>
           </div>
         </div>
         <div className="flex w-full px-3 py-4 h-10 rounded-md hover:bg-gray-200 ">
-          <div className="flex items-center justify-center gap-2">
-            <IoSettingsOutline fontSize={25} />
+          <div className="flex items-center justify-center gap-3">
+            <IoSettingsOutline fontSize={22} />
             <h1 className="text-lg">Setting</h1>
           </div>
         </div>
