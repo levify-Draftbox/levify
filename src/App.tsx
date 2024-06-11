bd836341dd028675fda56e92f3873a648814fae5
p4rthk4 <hello@parthka.dev>
2024-06-11 20:53:59 +0530

    [add] home page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}>
+        <Route path="/" element={<h1>Hello Page</h1>} />
         <Route path="/page1" element={<Page />} />
         <Route path="/page2" element={<Page />} />
         <Route path="/page3" element={<Page />} />
