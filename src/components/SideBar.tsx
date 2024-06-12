958b743aa11d722214eae498e3e0bf02cc160442
ravi <italiyaravi704@gmail.com>
2024-06-12 19:17:04 +0530

    [add]-user name

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -22,17 +22,14 @@ const SideBar = () => {
   return (
     <div className="flex flex-col justify-between h-full text-[#5f5e5b] font-semibold p-2 bg-[#f7f7f5] dark:bg-transparent">
       <div className="w-full flex flex-col mt-1">
-
-        <div className="flex w-full px-3 h-10 rounded-md hover:bg-[#ededeb] cursor-pointer dark:hover:bg-[#383838]">
-          <div className="flex items-center justify-center gap-3">
-            <IoSearch
-              fontSize={20}
-              className="text-[#91918e] dark:text-[#a3a3a3]"
-            />
-            <h1 className=" ">User</h1>
+        <div className="flex w-full px-3 h-10 rounded-md overflow-hidden hover:bg-[#ededeb] cursor-pointer dark:hover:bg-[#383838]">
+          <div className="flex items-center justify-center gap-3 flex-shrink-0">
+            <div className="w-5 h-5 rounded-sm bg-white"></div>
+            <p className="dark:text-[#d5d5d5] max-w-40 truncate">
+              user
+            </p>
           </div>
         </div>
-
 
         <div className="flex w-full px-3 py-4 h-10 rounded-md hover:bg-[#ededeb] cursor-pointer dark:hover:bg-[#383838]">
           <div className="flex items-center justify-center gap-3">
