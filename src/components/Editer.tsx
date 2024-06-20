7e6de65682547c4783374d76bd8adc0843a11f19
ravi <italiyaravi704@gmail.com>
2024-06-20 11:47:02 +0530

    commit

--- src/components/Editer.tsx
+++ src/components/Editer.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
 import EditorJS from "@editorjs/editorjs";
 import { useEffect, useRef } from "react";
 import List from "@editorjs/list";
