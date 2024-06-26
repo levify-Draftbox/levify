d8710e17b3f3e7e57d903592d29ce1404c4178ca
ravi <italiyaravi704@gmail.com>
2024-06-26 22:47:34 +0530

    [add]-toolbar

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,11 +1,18 @@
-import { CaretDown, CaretUp, DotsThree, SortAscending } from "@phosphor-icons/react";
+import {
+  CaretDown,
+  CaretLeft,
+  CaretRight,
+  CaretUp,
+  DotsThree,
+  SortAscending,
+} from "@phosphor-icons/react";
 import { useState } from "react";
 import { Button } from "./ui/button";
 
 const ToolBar = () => {
   const [UpDown, setUpDown] = useState(false);
   return (
-    <div className="w-full h-11 border-b-2 flex items-center justify-between px-5">
+    <div className="w-full h-11 border-b-2 flex items-center justify-between px-5 selection:select-none">
       <div className="flex gap-3 items-center">
         <div
           onClick={() => {
@@ -20,17 +27,34 @@ const ToolBar = () => {
             <CaretDown weight="fill" size={12} />
           )}
         </div>
-        <h2 className="font-bold text-lg">Inbox</h2>
+        <h2 className="font-semibold text-lg">Inbox</h2>
         <div className="py-1 px-1 rounded-md hover:bg-[rgba(0,0,0,0.07)] ">
           <DotsThree weight="bold" size={18} />
         </div>
       </div>
       <div className="flex items-center gap-3">
-        <Button variant={"toolbutton"} size={"toolsize"} to={"/inbox"} >All</Button>
-        <Button variant={"toolbutton"} size={"toolsize"} to={"/"} >Read</Button>
-        <Button variant={"toolbutton"} size={"toolsize"} to={"/"} >Unread</Button>
-        <Button variant={"toolbutton"} size={"toolsize"} to={"/"} >Has file</Button>
-        <SortAscending size={20}/>
+        <Button variant={"toolbutton"} size={"toolsize"} to={"/inbox"}>
+          All
+        </Button>
+        <Button variant={"toolbutton"} size={"toolsize"} to={"/"}>
+          Read
+        </Button>
+        <Button variant={"toolbutton"} size={"toolsize"} to={"/"}>
+          Unread
+        </Button>
+        <Button variant={"toolbutton"} size={"toolsize"} to={"/"}>
+          Has file
+        </Button>
+
+        <Button variant={"toolbutton"} size={"toolsize"}>
+        <SortAscending size={20} />
+        </Button>
+        
+        <div className="flex items-center gap-5 ml-2 text-[rgba(0,0,0,0.6)]">
+          <CaretLeft size={14} />
+          <p>1/1</p>
+          <CaretRight size={14} />
+        </div>
       </div>
     </div>
   );
