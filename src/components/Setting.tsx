5583492868b315d6a7cc83c16d3f4b683c9aa169
ravi <italiyaravi704@gmail.com>
2024-07-10 14:41:38 +0530

    [add]-your plan in settings

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 import { ResizableHandle, ResizablePanel } from "./ui/resizable";
 import { motion } from "framer-motion";
 import { useState } from "react";
-
+import { Progress } from "@/components/ui/progress";
 type SearchBarProp = {
   onSettingToggle: () => void;
 };
@@ -12,12 +12,12 @@ const Setting = ({ onSettingToggle }: SearchBarProp) => {
   return (
     <>
       <ResizableHandle />
-      <ResizablePanel defaultSize={19}>
+      <ResizablePanel defaultSize={20}>
         <motion.div
           initial={{ x: 50, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ stiffness: 200, duration: 0.25 }}
-          className="h-full bg-inbox-bg"
+          className="h-full bg-inbox-bg select-none"
         >
           <div className="p-3 ">
             <div className=" flex justify-between">
@@ -43,7 +43,6 @@ const Setting = ({ onSettingToggle }: SearchBarProp) => {
 const SettingSidebar = () => {
   const [toggleLayout, settoggleLayout] = useState(false);
   const [toggleDensity, settoggleDensity] = useState(false);
-  const [togglecomposer, settogglecomposer] = useState(false);
 
   return (
     <div>
@@ -53,8 +52,32 @@ const SettingSidebar = () => {
       <Button variant={"whiteButton"} className="mt-2">
         Get the Rellite Mail app
       </Button>
+
+      <div className="bg-white mt-3  items-center rounded-lg justify-center w-full p-2 text-primary shadow dark:bg-[rgba(250,250,250,0.1)]">
+        <div className="px-2 flex flex-col gap-3 mt-2 mb-3">
+        <div className="text-center mb-1">
+            <h2 className="font-semibold">Your Plan</h2>
+          </div>
+          <div className="w-full pt-1 px-1">
+            <Progress value={10} />
+            <div className="text-xs flex justify-between  mt-2">
+              <div className="flex">
+                <span className="font-semibold">200 MB </span>
+                <p>/ 2.0 GB</p>
+              </div>
+            </div>
+          </div>
+          <Button variant={"superActive"} className="capitalize">
+            upgrad your plan
+          </Button>
+        </div>
+      </div>
+
       <div className="bg-white mt-3 items-center rounded-lg justify-center w-full p-2 text-primary shadow dark:bg-[rgba(250,250,250,0.1)]">
         <div className="px-2 flex flex-col gap-4">
+          <div className="text-center mb-1">
+            <h2 className="font-semibold">Personalization</h2>
+          </div>
           <div
             className="flex justify-between cursor-pointer"
             onClick={() => settoggleLayout(!toggleLayout)}
@@ -65,49 +88,54 @@ const SettingSidebar = () => {
 
           {toggleLayout && (
             <motion.div
-            initial={{y:-20,opacity:0}}
-            animate={{y:0,opacity:1}}
-            className="flex gap-4">
+              initial={{ y: -20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              className="flex gap-4"
+            >
               <div className="w-28 h-16 bg-black"></div>
               <div className="w-28 h-16 bg-core"></div>
             </motion.div>
           )}
 
-          <div className="flex justify-between cursor-pointer"
-          onClick={() => settoggleDensity(!toggleDensity)}>
+          <div
+            className="flex justify-between cursor-pointer"
+            onClick={() => settoggleDensity(!toggleDensity)}
+          >
             <p>Density</p>
             <p className="text-core text-sm capitalize">comfortable</p>
           </div>
 
           {toggleDensity && (
             <motion.div
-            initial={{y:-20,opacity:0}}
-            animate={{y:0,opacity:1}}
-            className="flex gap-4">
+              initial={{ y: -20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              className="flex gap-4"
+            >
               <div className="w-28 h-16 bg-black"></div>
               <div className="w-28 h-16 bg-core"></div>
             </motion.div>
           )}
-          <div className="flex justify-between cursor-pointer"
-          onClick={() => settogglecomposer(!togglecomposer)}>
-            <p>composer</p>
-            <p className="text-core text-sm capitalize">Normal</p>
-          </div>
 
-          {togglecomposer && (
-            <motion.div
-            initial={{y:-20,opacity:0}}
-            animate={{y:0,opacity:1}}
-            className="flex gap-4">
-              <div className="w-28 h-16 bg-black"></div>
-              <div className="w-28 h-16 bg-core"></div>
-            </motion.div>
-          )}
+          <div className="cursor-pointer">
+            <p>
+              <strong>Theme: </strong>Rellite
+            </p>
+            {/* <div className="flex justify-between mt-2">
+              <p>Sync with system</p>
+              <label className="inline-flex items-center cursor-pointer">
+                <input type="checkbox" value="" className="sr-only peer" />
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
+              </label>
+            </div> */}
+          </div>
         </div>
       </div>
 
       <div className="bg-white mt-3 items-center rounded-lg justify-center w-full p-2 text-primary shadow dark:bg-[rgba(250,250,250,0.1)]">
         <div className="px-2 flex flex-col gap-6 mt-2">
+        <div className="text-center mb-1">
+            <h2 className="font-semibold">Preferences</h2>
+          </div>
           <div className="flex justify-between cursor-pointer">
             <p>Keyboard shortcuts</p>
             <label className="inline-flex items-center cursor-pointer">
@@ -122,20 +150,34 @@ const SettingSidebar = () => {
               <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
             </label>
           </div>
-
-          <div className="cursor-pointer">
-            <p>
-              <strong>Theme: </strong>Rellite
-            </p>
-            <div className="flex justify-between mt-2">
-              <p>Sync with system</p>
-              <label className="inline-flex items-center cursor-pointer">
-                <input type="checkbox" value="" className="sr-only peer" />
-                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
-              </label>
-            </div>
+          <div
+            className="flex justify-between cursor-pointer"
+          >
+            <p>Composer Full Screen</p>
+            <label className="inline-flex items-center cursor-pointer">
+              <input type="checkbox" value="" className="sr-only peer" />
+              <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
+            </label>
           </div>
         </div>
+      </div>
+
+      <div className="flex flex-wrap gap-1 p-2 mt-2">
+        <Button variant={"link"} size={"toolsize"}>
+          About .
+        </Button>
+        <Button variant={"link"} size={"toolsize"}>
+          Help .
+        </Button>
+        <Button variant={"link"} size={"toolsize"}>
+          Contact .
+        </Button>
+        <Button variant={"link"} size={"toolsize"}>
+          Rellite .
+        </Button>
+        <Button variant={"link"} size={"toolsize"}>
+          Privacy And Polacy .
+        </Button>
       </div>
     </div>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,7 +18,7 @@ const button = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "text-core underline-offset-4 hover:underline",
         active:
           "bg-transparant items-center justify-left gap-2 px-3  w-full text-active-foreground hover:bg-active hover:shadow",
         superActive:
