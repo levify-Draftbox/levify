0b0605a95ec20c62e06a7de5625ed32d03a26dba
ravi <italiyaravi704@gmail.com>
2024-07-11 18:48:15 +0530

    rfgerg

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -2,7 +2,7 @@ import { ArrowRight } from "@phosphor-icons/react";
 import { Button } from "./ui/button";
 import { ResizableHandle, ResizablePanel } from "./ui/resizable";
 import { motion } from "framer-motion";
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
 import { Progress } from "@/components/ui/progress";
 type SearchBarProp = {
   onSettingToggle: () => void;
@@ -46,10 +46,9 @@ const SettingSidebar = () => {
 
   const [selectedImage, setSelectedImage] = useState(null);
 
-  const handleImageClick = (index: number | SetStateAction<null>) => {
+  const handleImageClick = (index) => {
     setSelectedImage(index);
   };
-
 
   return (
     <div>
@@ -62,7 +61,7 @@ const SettingSidebar = () => {
 
       <div className="bg-white mt-3  items-center rounded-lg justify-center w-full p-2 text-primary shadow dark:bg-[rgba(250,250,250,0.1)]">
         <div className="px-2 flex flex-col gap-3 mt-2 mb-3">
-        <div className="text-center mb-1">
+          <div className="text-center mb-1">
             <h2 className="font-semibold">Your Plan</h2>
           </div>
           <div className="w-full pt-1 px-1">
@@ -94,24 +93,28 @@ const SettingSidebar = () => {
           </div>
 
           {toggleLayout && (
-             <motion.div
-             initial={{ y: -20, opacity: 0 }}
-             animate={{ y: 0, opacity: 1 }}
-             className="flex gap-4"
-           >
-             <img
-               src="./download (1).jpg"
-               className={`w-28 h-16 object-cover rounded-md ${selectedImage === 1 ? 'ring-2 ring-core' : ''}`}
-               onClick={() => handleImageClick(1)}
-               alt="Image 1"
-             />
-             <img
-               src="./download.jpg"
-               className={`w-28 h-16 object-cover rounded-md ${selectedImage === 2 ? 'ring-2 ring-core' : ''}`}
-               onClick={() => handleImageClick(2)}
-               alt="Image 2"
-             />
-           </motion.div>
+            <motion.div
+              initial={{ y: -20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              className="flex gap-4"
+            >
+              <img
+                src="./download (1).jpg"
+                className={`w-28 h-16 object-cover rounded-md ${
+                  selectedImage === 1 ? "ring-2 ring-core" : ""
+                }`}
+                onClick={() => handleImageClick(1)}
+                alt="Image 1"
+              />
+              <img
+                src="./download.jpg"
+                className={`w-28 h-16 object-cover rounded-md ${
+                  selectedImage === 2 ? "ring-2 ring-core" : ""
+                }`}
+                onClick={() => handleImageClick(2)}
+                alt="Image 2"
+              />
+            </motion.div>
           )}
 
           <div
@@ -124,23 +127,27 @@ const SettingSidebar = () => {
 
           {toggleDensity && (
             <motion.div
-            initial={{ y: -20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            className="flex gap-4"
-          >
-            <img
-              src="./download (1).jpg"
-              className={`w-28 h-16 object-cover rounded-md ${selectedImage === 1 ? 'ring-2 ring-core' : ''}`}
-              onClick={() => handleImageClick(1)}
-              alt="Image 1"
-            />
-            <img
-              src="./download.jpg"
-              className={`w-28 h-16 object-cover rounded-md ${selectedImage === 2 ? 'ring-2 ring-core' : ''}`}
-              onClick={() => handleImageClick(2)}
-              alt="Image 2"
-            />
-          </motion.div>
+              initial={{ y: -20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              className="flex gap-4"
+            >
+              <img
+                src="./download (1).jpg"
+                className={`w-28 h-16 object-cover rounded-md ${
+                  selectedImage === 1 ? "ring-2 ring-core" : ""
+                }`}
+                onClick={() => handleImageClick(1)}
+                alt="Image 1"
+              />
+              <img
+                src="./download.jpg"
+                className={`w-28 h-16 object-cover rounded-md ${
+                  selectedImage === 2 ? "ring-2 ring-core" : ""
+                }`}
+                onClick={() => handleImageClick(2)}
+                alt="Image 2"
+              />
+            </motion.div>
           )}
 
           <div className="cursor-pointer">
@@ -160,7 +167,7 @@ const SettingSidebar = () => {
 
       <div className="bg-white mt-3 items-center rounded-lg justify-center w-full p-2 text-primary shadow dark:bg-[rgba(250,250,250,0.1)]">
         <div className="px-2 flex flex-col gap-6 mt-2">
-        <div className="text-center mb-1">
+          <div className="text-center mb-1">
             <h2 className="font-semibold">Preferences</h2>
           </div>
           <div className="flex justify-between cursor-pointer">
@@ -177,9 +184,7 @@ const SettingSidebar = () => {
               <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
             </label>
           </div>
-          <div
-            className="flex justify-between cursor-pointer"
-          >
+          <div className="flex justify-between cursor-pointer">
             <p>Composer Full Screen</p>
             <label className="inline-flex items-center cursor-pointer">
               <input type="checkbox" value="" className="sr-only peer" />
@@ -190,19 +195,19 @@ const SettingSidebar = () => {
       </div>
 
       <div className="flex flex-wrap gap-1 p-2 mt-2">
-        <Button variant={"link"} size={"toolsize"} >
+        <Button variant={"link"} size={"toolsize"}>
           About .
         </Button>
-        <Button variant={"link"} size={"toolsize"} >
+        <Button variant={"link"} size={"toolsize"}>
           Help .
         </Button>
-        <Button variant={"link"} size={"toolsize"} >
+        <Button variant={"link"} size={"toolsize"}>
           Contact .
         </Button>
-        <Button variant={"link"} size={"toolsize"} >
+        <Button variant={"link"} size={"toolsize"}>
           Rellite .
         </Button>
-        <Button variant={"link"} size={"toolsize"} >
+        <Button variant={"link"} size={"toolsize"}>
           Privacy And Polacy .
         </Button>
       </div>
