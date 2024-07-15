8d879f18522fb9473982cc3fc190f097323ffe4c
ravi <italiyaravi704@gmail.com>
2024-07-15 20:09:57 +0530

    [chenge]-convert into card component

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -1,7 +1,7 @@
 import { Button } from "./ui/button";
 import { ResizableHandle, ResizablePanel } from "./ui/resizable";
 import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { Progress } from "@/components/ui/progress";
 import Modal from "./Model"; // Make sure to import Modal correctly
 import { ChevronRight } from "lucide-react";
@@ -93,187 +93,166 @@ const SettingSidebar = () => {
         />
       </div>
 
-      <div className="bg-white mt-3 items-center rounded-lg justify-center w-full p-2 text-primary shadow dark:bg-[rgba(250,250,250,0.1)]">
-        <div className="px-2 flex flex-col gap-3 mt-2 mb-3">
-          <div className="mb-1">
-            <h2 className="text-xs text-gray-700 dark:text-gray-400">
-              Your Plan
-            </h2>
-          </div>
-          <div className="w-full pt-1 px-1">
-            <Progress value={10} />
-            <div className="text-xs flex justify-between mt-2">
-              <div className="flex">
-                <span className="font-semibold">200 MB </span>
-                <p>/ 2.0 GB</p>
-              </div>
+      <Card hedding={"Your plan"}>
+        <div className="w-full pt-1 px-1">
+          <Progress value={10} />
+          <div className="text-xs flex justify-between mt-2">
+            <div className="flex">
+              <span className="font-semibold">200 MB </span>
+              <p>/ 2.0 GB</p>
             </div>
           </div>
-          <Button variant={"superActive"} className="capitalize">
-            Upgrade your plan
-          </Button>
         </div>
-      </div>
+        <Button variant={"superActive"} className="capitalize">
+          Upgrade your plan
+        </Button>
+      </Card>
 
-      <div className="bg-white mt-3 items-center rounded-lg justify-center w-full p-2 text-primary shadow dark:bg-[rgba(250,250,250,0.1)]">
-        <div className="px-2 flex flex-col gap-4 mt-2 mb-3">
-          <div className="">
-            <h2 className="text-xs text-gray-700 dark:text-gray-400">
-              Personalization
-            </h2>
-          </div>
-          <div
-            className="flex justify-between cursor-pointer"
-            onClick={() => setToggleLayout(!toggleLayout)}
+      <Card hedding={"Personalization"}>
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={() => setToggleLayout(!toggleLayout)}
+        >
+          <p>Layout</p>
+          <p className="text-core text-sm capitalize">row</p>
+        </div>
+
+        {toggleLayout && (
+          <motion.div
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut" }}
+            className="flex gap-4"
           >
-            <p>Layout</p>
-            <p className="text-core text-sm capitalize">row</p>
-          </div>
+            <img
+              src="./download (1).jpg"
+              style={{
+                width: "calc((100% - 1rem) / 2)",
+              }}
+              className={`h-auto object-cover rounded-md ${
+                selectedImage === 1 ? "ring-2 ring-core" : ""
+              }`}
+              onClick={() => handleImageClick(1)}
+              alt="Image 1"
+            />
+            <img
+              src="./download.jpg"
+              style={{
+                width: "calc((100% - 1rem) / 2)",
+              }}
+              className={`h-auto object-cover rounded-md ${
+                selectedImage === 2 ? "ring-2 ring-core" : ""
+              }`}
+              onClick={() => handleImageClick(2)}
+              alt="Image 2"
+            />
+          </motion.div>
+        )}
 
-          {toggleLayout && (
-            <motion.div
-              initial={{ y: -20, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{ ease: "easeInOut" }}
-              className="flex gap-4"
-            >
-              <img
-                src="./download (1).jpg"
-                style={{
-                  width: "calc((100% - 1rem) / 2)",
-                }}
-                className={`h-auto object-cover rounded-md ${
-                  selectedImage === 1 ? "ring-2 ring-core" : ""
-                }`}
-                onClick={() => handleImageClick(1)}
-                alt="Image 1"
-              />
-              <img
-                src="./download.jpg"
-                style={{
-                  width: "calc((100% - 1rem) / 2)",
-                }}
-                className={`h-auto object-cover rounded-md ${
-                  selectedImage === 2 ? "ring-2 ring-core" : ""
-                }`}
-                onClick={() => handleImageClick(2)}
-                alt="Image 2"
-              />
-            </motion.div>
-          )}
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={() => setToggleDensity(!toggleDensity)}
+        >
+          <p>Density</p>
+          <p className="text-core text-sm capitalize">comfortable</p>
+        </div>
 
-          <div
-            className="flex justify-between cursor-pointer"
-            onClick={() => setToggleDensity(!toggleDensity)}
+        {toggleDensity && (
+          <motion.div
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut" }}
+            className="flex gap-4"
           >
-            <p>Density</p>
-            <p className="text-core text-sm capitalize">comfortable</p>
-          </div>
+            <img
+              src="./download (1).jpg"
+              style={{
+                width: "calc((100% - 1rem) / 2)",
+              }}
+              className={`h-auto object-cover rounded-md ${
+                selectedImage === 1 ? "ring-2 ring-core" : ""
+              }`}
+              onClick={() => handleImageClick(1)}
+              alt="Image 1"
+            />
+            <img
+              src="./download.jpg"
+              style={{
+                width: "calc((100% - 1rem) / 2)",
+              }}
+              className={`h-auto object-cover rounded-md ${
+                selectedImage === 2 ? "ring-2 ring-core" : ""
+              }`}
+              onClick={() => handleImageClick(2)}
+              alt="Image 2"
+            />
+          </motion.div>
+        )}
 
-          {toggleDensity && (
-            <motion.div
-              initial={{ y: -20, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{ ease: "easeInOut" }}
-              className="flex gap-4"
-            >
-              <img
-                src="./download (1).jpg"
-                style={{
-                  width: "calc((100% - 1rem) / 2)",
-                }}
-                className={`h-auto object-cover rounded-md ${
-                  selectedImage === 1 ? "ring-2 ring-core" : ""
-                }`}
-                onClick={() => handleImageClick(1)}
-                alt="Image 1"
-              />
-              <img
-                src="./download.jpg"
-                style={{
-                  width: "calc((100% - 1rem) / 2)",
-                }}
-                className={`h-auto object-cover rounded-md ${
-                  selectedImage === 2 ? "ring-2 ring-core" : ""
-                }`}
-                onClick={() => handleImageClick(2)}
-                alt="Image 2"
-              />
-            </motion.div>
-          )}
-
-          <div className="cursor-pointer">
-            <p>
-              <strong>Theme: </strong>Rellite
-            </p>
-            {/* <div className="flex justify-between mt-2">
+        <div className="cursor-pointer">
+          <p>
+            <strong>Theme: </strong>Rellite
+          </p>
+          {/* <div className="flex justify-between mt-2">
               <p>Sync with system</p>
               <label className="inline-flex items-center cursor-pointer">
                 <input type="checkbox" value="" className="sr-only peer" />
                 <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
               </label>
             </div> */}
-          </div>
+        </div>
 
-          <div className="cursor-pointer">
-            <p>
-              <strong>Colors: </strong>
-            </p>
-            <div className="flex gap-3 mt-2">
-              <div className="flex space-x-2">
-                {[
-                  "bg-red-600",
-                  "bg-green-600",
-                  "bg-blue-600",
-                  "bg-black",
-                  "bg-yellow-600",
-                ].map((color, index) => (
-                  <div
-                    key={index}
-                    className={`w-10 h-10 rounded-full ${color} ${
-                      selectedIndex === index
-                        ? "ring-2 ring-offset-1 ring-core"
-                        : ""
-                    }`}
-                    onClick={() => handleDivClick(index)}
-                  ></div>
-                ))}
-              </div>
+        <div className="cursor-pointer">
+          <p>
+            <strong>Colors: </strong>
+          </p>
+          <div className="flex gap-3 mt-2">
+            <div className="flex space-x-2">
+              {[
+                "bg-red-600",
+                "bg-green-600",
+                "bg-blue-600",
+                "bg-black",
+                "bg-yellow-600",
+              ].map((color, index) => (
+                <div
+                  key={index}
+                  className={`w-10 h-10 rounded-full ${color} ${
+                    selectedIndex === index
+                      ? "ring-2 ring-offset-1 ring-core"
+                      : ""
+                  }`}
+                  onClick={() => handleDivClick(index)}
+                ></div>
+              ))}
             </div>
           </div>
         </div>
-      </div>
+      </Card>
 
-      <div className="bg-white mt-3 items-center rounded-lg justify-center w-full p-2 text-primary shadow dark:bg-[rgba(250,250,250,0.1)]">
-        <div className="px-2 flex flex-col gap-4 mt-2 mb-3">
-          <div className="">
-            <h2 className="text-xs text-gray-700 dark:text-gray-400">
-              Preferences
-            </h2>
-          </div>
-          <div className="flex justify-between cursor-pointer">
-            <p>Keyboard shortcuts</p>
-            <label className="inline-flex items-center cursor-pointer">
-              <input type="checkbox" value="" className="sr-only peer" />
-              <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
-            </label>
-          </div>
-          <div className="flex justify-between cursor-pointer">
-            <p>Beta Access</p>
-            <label className="inline-flex items-center cursor-pointer">
-              <input type="checkbox" value="" className="sr-only peer" />
-              <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
-            </label>
-          </div>
-          <div className="flex justify-between cursor-pointer">
-            <p>Composer Full Screen</p>
-            <label className="inline-flex items-center cursor-pointer">
-              <input type="checkbox" value="" className="sr-only peer" />
-              <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
-            </label>
-          </div>
+      <Card hedding={"Preferences"}>
+        <div className="flex justify-between cursor-pointer">
+          <p>Keyboard shortcuts</p>
+          <label className="inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" className="sr-only peer" />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
+          </label>
         </div>
-      </div>
+        <div className="flex justify-between cursor-pointer">
+          <p>Beta Access</p>
+          <label className="inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" className="sr-only peer" />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
+          </label>
+        </div>
+        <div className="flex justify-between cursor-pointer">
+          <p>Composer Full Screen</p>
+          <label className="inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" className="sr-only peer" />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-core"></div>
+          </label>
+        </div>
+      </Card>
 
       <div className="flex flex-wrap gap-1 p-2 mt-2 justify-center text-xs text-gray-400">
         <a
@@ -321,5 +300,27 @@ const SettingSidebar = () => {
 };
 
 export { SettingSidebar };
+
+interface card {
+  hedding: string;
+  children: ReactNode;
+}
+
+const Card = ({ hedding, children }:card) => {
+  return (
+    <div className="bg-white mt-3 items-center rounded-lg justify-center w-full p-2 text-primary shadow dark:bg-[rgba(250,250,250,0.1)]">
+      <div className="px-2 flex flex-col gap-4 mt-2 mb-3">
+        <div className="">
+          <h2 className="text-xs font-medium  text-gray-700 dark:text-gray-400">
+            {hedding}
+          </h2>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export { Card };
 
 export default Setting;
