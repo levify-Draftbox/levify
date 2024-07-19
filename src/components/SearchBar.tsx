032d7e4dc537d5c0958cf1793d5fd8d5e7fcf3b2
p4rthk4 <hello@parthka.dev>
2024-07-19 19:03:56 +0530

    [add] tooltip

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,6 @@
 import { Bell, Gear, MagnifyingGlass } from "@phosphor-icons/react";
 import { Button } from "./ui/button";
+import { Tooltip } from "./ui/tooltip";
 
 type SearchBarProp = {
   onSettingToggle: () => void
@@ -19,15 +20,24 @@ const SearchBar = ({ onSettingToggle }: SearchBarProp) => {
           />
         </div>
         <div className="flex items-center gap-2">
-          <Button variant={"toolbutton"} className="" onClick={() => onSettingToggle()}>
-            <Bell size={20} />
-          </Button>
-          <Button variant={"toolbutton"} className="" onClick={() => onSettingToggle()}>
-            <Gear size={20} />
-          </Button>
-          <div className="w-9 h-9 border-2 cursor-pointer rounded-lg flex items-center justify-center hover:bg-[rgba(0,0,0,0.07)] dark:hover:bg-[rgba(250,250,250,0.2)]">
-            <p>R</p>
-          </div>
+
+          <Tooltip tip="Notification">
+            <Button variant={"toolbutton"} className="" onClick={() => onSettingToggle()}>
+              <Bell size={20} />
+            </Button>
+          </Tooltip>
+
+          <Tooltip tip="Settings">
+            <Button variant={"toolbutton"} className="" onClick={() => onSettingToggle()}>
+              <Gear size={20} />
+            </Button>
+          </Tooltip>
+
+          <Tooltip tip="Your Profile">
+            <div className="w-9 h-9 border-2 cursor-pointer rounded-lg flex items-center justify-center hover:bg-[rgba(0,0,0,0.07)] dark:hover:bg-[rgba(250,250,250,0.2)]">
+              <p>R</p>
+            </div>
+          </Tooltip>
         </div>
       </div>
     </div>
