f1a2f102b0b5071f654ff2c12e54258db72aad92
p4rthk4 <hello@parthka.dev>
2024-07-19 19:03:46 +0530

    [add] tooltip and modify

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./components/Theme-provider";
 import Login from "./Auth/Login";
 import Inbox from "./page/Inbox";
+import { TooltipProvider } from "./components/ui/tooltip";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
+      <TooltipProvider>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />}>
@@ -15,6 +17,7 @@ function App() {
           <Route path="/*" element={<Inbox />} />
         </Route>
       </Routes>
+      </TooltipProvider>
     </ThemeProvider>
   );
 }
