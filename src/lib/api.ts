43ee0da7de611a3dfe1edfbd2b9f17191dcbb73e
Parthka <hello@parthka.dev>
2024-07-24 13:36:23 +0530

    fix api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3030", // Your API base URL
+  baseURL: "http://localhost:3030/api", // Your API base URL
   timeout: 10000, // Request timeout in milliseconds
   headers: {
     "Content-Type": "application/json",
