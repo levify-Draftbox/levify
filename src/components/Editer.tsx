ae9083c2519cbb0654a7cc71100053c65643a072
raviitaliya <italiyaravi704@gmail.com>
2024-08-08 19:07:55 +0530

    [add]-convert html

--- src/components/Editer.tsx
+++ src/components/Editer.tsx
@@ -1,6 +1,15 @@
 import EditorJS, { EditorConfig } from "@editorjs/editorjs";
 import { useEffect, useRef } from "react";
 import edjsHTML from "editorjs-html";
+import Header from '@editorjs/header';
+import LinkTool from '@editorjs/link';
+import RawTool from '@editorjs/raw';
+import ImageTool from '@editorjs/image';
+import Checklist from '@editorjs/checklist'
+import List from "@editorjs/list";
+import Embed from '@editorjs/embed';
+import Quote from '@editorjs/quote';
+
 
 const Editor: React.FC = () => {
   const editorRef = useRef<HTMLDivElement>(null);
@@ -21,8 +30,33 @@ const Editor: React.FC = () => {
           }
         },
         tools: {
-          // We'll leave this empty for now
+          header: Header,
+          linkTool: LinkTool,
+          raw: RawTool,
+          image: {
+            class: ImageTool,
+            config: {
+              endpoints: {
+                byFile: 'http://localhost:8008/uploadFile',
+                byUrl: 'http://localhost:8008/fetchUrl',
+              },
+            },
+          },
+          checklist: {
+            class: Checklist,
+            inlineToolbar: true,
+          },
+          list: {
+            class: List,
+            inlineToolbar: true,
+            config: {
+              defaultStyle: 'unordered',
+            },
+          },
+          embed: Embed,
+          quote: Quote,
         },
+        
         data: {
           time: new Date().getTime(),
           blocks: [
---
+++ types/editorjs-embed.d.ts
@@ -0,0 +1,4 @@
+declare module '@editorjs/embed' {
+    export * from '@editorjs/embed/dist/index.d.ts';
+  }
+  
