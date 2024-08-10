553cb4b0599b12452fd5f0c13de409245b11ed12
p4rthk4 <hello@parthka.dev>
2024-08-10 16:05:35 +0530

    [add] inputes

--- src/page/Inputs.tsx
+++ src/page/Inputs.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { PhoneInput } from "@/components/ui/phone";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
@@ -57,6 +58,21 @@ function Inputs(): React.ReactNode {
 
                 <div className="px-4 py-1">
                     <PhoneInput placeholder="Phone No" defaultCountry="IN" label="Phone No" />
+                </div>
+
+                <div className="items-top flex space-x-2 px-4 py-4">
+                    <Checkbox id="terms1" />
+                    <div className="grid gap-1.5 leading-none">
+                        <label
+                            htmlFor="terms1"
+                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                            Accept terms and conditions
+                        </label>
+                        <p className="text-sm text-muted-foreground">
+                            You agree to our Terms of Service and Privacy Policy.
+                        </p>
+                    </div>
                 </div>
 
                 <div className="px-4 py-3">
