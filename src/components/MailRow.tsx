ea51991fd200bccd4b9e459148ae8f72897fbe2b
parth <hello@parthka.dev>
2024-08-24 12:32:05 +0530

    if subjhect is not found than show no subject and add text

--- src/components/MailRow.tsx
+++ src/components/MailRow.tsx
@@ -1,7 +1,6 @@
 import { Envelope, TrashSimple } from "@phosphor-icons/react";
 import { Archive, Clock, Star } from "lucide-react";
 import { Button } from "./ui/button";
-import { useState } from "react";
 import {
   ContextMenu,
   ContextMenuContent,
@@ -14,56 +13,56 @@ import { cn } from "@/lib/utils";
 
 const Mail: React.FC<Partial<{
   onClick: () => void
-} & EmailObject>> = ({ onClick, b_subject, b_from, from_profile, b_datetime, unread, b_from_name }) => {
-  const [IsHovered, setIsHovered] = useState(false);
-
+} & EmailObject>> = ({ onClick, b_subject, b_from, from_profile, b_datetime, unread, b_from_name, b_text }) => {
   return (
     <ContextMenu>
       <ContextMenuTrigger>
         <div
           onClick={() => onClick && onClick()}
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
           className={cn(
-            "w-full h-[50px] border-b py-3 px-4 flex justify-between items-center hover:shadow-md cursor-pointer text-[15px] font-thin",
+            "group",
+            "w-full h-[50px] border-b py-3 px-4 flex justify-between items-center cursor-pointer text-[15px] font-thin",
             unread ? "font-medium" : ""
           )}
         >
-          <div className="flex flex-1 items-center gap-4">
-            <div className="w-7 h-7 border overflow-hidden rounded-md flex items-center justify-center">
+          <div className="w-[75%] flex flex-1 items-center gap-4">
+            <div className="min-w-7 w-7 h-7 border overflow-hidden rounded-md flex items-center justify-center">
               <img src={from_profile} className="h-full w-full" />
             </div>
 
-            <Button variant={"star"} size={"toolsize"}>
+            <Button className="" variant={"star"} size={"toolsize"}>
               <Star size={15} />
             </Button>
-            <p className="w-[18%] overflow-hidden">{getUserName(b_from_name, b_from as string)}</p>
-            <p className="font-thin flex items-center">
-              {unread && <div className="w-[10px] h-[10px] bg-core rounded-full mr-3"></div>} {b_subject}
+            <div className="min-w-[130px] w-[18%]">
+            <p className="w-full overflow-hidden">{getUserName(b_from_name, b_from as string)}</p>
+            </div>
+            <p className="max-w-[60%] font-thin flex items-center whitespace-nowrap overflow-hidden">
+              {unread && <div className="w-[10px] h-[10px] bg-core rounded-full mr-3"></div>}
+              {b_subject ? b_subject : <span className="text-gray-500 dark:text-gray-400 italic">No Subject</span>}
+              {(b_text && b_text != "") && <span className="text-gray-500 dark:text-gray-300 overflow-hidden text-ellipsis">&nbsp;- {b_text.split("\n")[0].split(" ").splice(0, 7).join(" ")}...</span>}
             </p>
           </div>
-          <div className="w-[10%] text-right">
-            {IsHovered ? (
-              <div className="flex justify-end">
-                <Button variant={"toolbutton"}>
-                  <Envelope size={17} />
-                </Button>
-                <Button variant={"toolbutton"}>
-                  <TrashSimple weight="bold" size={17} />
-                </Button>
-                <Button variant={"toolbutton"}>
-                  <Archive size={17} />
-                </Button>
-                <Button variant={"toolbutton"}>
-                  <Clock size={17} />
-                </Button>
-              </div>
-            ) : (
-              <p className="text-sm font-thin">
-                {moment(new Date(b_datetime || "")).fromNow()}
-              </p>
-            )}
+
+          <div className="w-[20%] text-right">
+            <div className="hidden justify-end group-hover:flex">
+              <Button variant={"toolbutton"}>
+                <Envelope size={17} />
+              </Button>
+              <Button variant={"toolbutton"}>
+                <TrashSimple weight="bold" size={17} />
+              </Button>
+              <Button variant={"toolbutton"}>
+                <Archive size={17} />
+              </Button>
+              <Button variant={"toolbutton"}>
+                <Clock size={17} />
+              </Button>
+            </div>
+            <p className="text-sm font-thin group-hover:hidden">
+              {moment(new Date(b_datetime || "")).fromNow()}
+            </p>
           </div>
+
         </div>
       </ContextMenuTrigger>
 
