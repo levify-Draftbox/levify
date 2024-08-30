7069c89d8c73f103b84ea8bc39b2d5ddb249e9a7
parth <hello@parthka.dev>
2024-08-30 09:17:42 +0530

    split ui

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions:{
+      output:{
+        manualChunks: (id) => {
+          if (id.includes('radix-ui')) return 'ui'
+        }
+      }
+    }
+  }
 })
